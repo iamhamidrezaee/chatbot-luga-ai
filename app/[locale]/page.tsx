edb61afbd5a1df98ca3115b7984e93c0b08760ee
Hamid Rezaee <137823856+iamhamidrezaee@users.noreply.github.com>
2024-06-19 18:11:28 -0400

    Add files via upload

---
+++ app/[locale]/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { ChatbotUISVG } from "@/components/icons/chatbotui-svg"
+import { IconArrowRight } from "@tabler/icons-react"
+import { useTheme } from "next-themes"
+import Link from "next/link"
+
+export default function HomePage() {
+  const { theme } = useTheme()
+
+  return (
+    <div className="flex size-full flex-col items-center justify-center">
+
+      <div className="mt-2 text-5xl tracking-wide">Luga AI</div>
+
+      <Link
+        className="mt-10 flex w-[200px] items-center justify-center rounded-lg bg-purple-400 p-2"
+        href="/login"
+      >
+        Start Chatting
+        <IconArrowRight className="ml-1" size={20} />
+      </Link>
+    </div>
+  )
+}
